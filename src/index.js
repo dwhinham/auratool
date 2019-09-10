2a3a64f7827687ce6c86c2ddeb6405e261372647
Dale Whinham <daleyo@gmail.com>
2019-09-10 18:00:55 +0100

    Update to TypeScript project template

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 import UtilSim from './utilsim'
+import * as serviceWorker from './serviceWorker';
 
 // Setup Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -41,3 +42,8 @@ ReactDOM.render(
 	</MathJax.Context>,
 	document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/utilsim.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import UtilSim from './utilsim';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<UtilSim />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
